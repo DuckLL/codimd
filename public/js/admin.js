aacfc6bd61b74420342eeb124eb4765f5cd4d897
DuckLL <duckll@users.noreply.github.com>
2024-11-07 16:21:21 +0800

    lint file & fix admin page & confirm change password

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,50 +1,58 @@
-require("../css/admin.css");
+/* eslint-env browser, jquery */
+
+require('../css/admin.css')
 
 $(document).ready(function () {
   // Toggle admin
-  $(".toggle-admin").click(function () {
-    var $button = $(this);
-    var userId = $button.closest(".flex-table-row").data("user-id");
-    var isAdmin = $button.data("admin") === "true";
+  $('.toggle-admin').click(function () {
+    var $button = $(this)
+    var userId = $button.closest('.flex-table-row').data('user-id')
+    var isAdmin = $button.data('admin') === 'true'
 
     $.ajax({
-      url: "/admin/setAdmin",
-      type: "POST",
+      url: '/admin/setAdmin',
+      type: 'POST',
       data: JSON.stringify({ userId: userId, admin: !isAdmin }),
-      contentType: "application/json",
+      contentType: 'application/json',
       success: function () {
         if (isAdmin) {
-          $button.removeClass("glyphicon-remove").addClass("glyphicon-ok");
-          $button.data("admin", "false");
-          $button.prev("label").text("No");
+          $button.removeClass('glyphicon-remove').addClass('glyphicon-ok')
+          $button.data('admin', 'false')
+          $button.prev('label').text('No')
         } else {
-          $button.removeClass("glyphicon-ok").addClass("glyphicon-remove");
-          $button.data("admin", "true");
-          $button.prev("label").text("Yes");
+          $button.removeClass('glyphicon-ok').addClass('glyphicon-remove')
+          $button.data('admin', 'true')
+          $button.prev('label').text('Yes')
         }
       },
       error: function (error) {
-        alert("Failed to toggle admin status");
-      },
-    });
-  });
+        alert(error)
+      }
+    })
+  })
 
   // Reset password
-  $(".reset-password").click(function () {
-    var userId = $(this).closest(".flex-table-row").data("user-id");
+  $('.reset-password').click(function () {
+    var userId = $(this).closest('.flex-table-row').data('user-id')
 
-    $.ajax({
-      url: "/admin/resetPassword",
-      type: "POST",
-      data: JSON.stringify({ userId: userId }),
-      contentType: "application/json",
-      dataType: "json",
-      success: function (response) {
-        alert("New Password: " + response.newPassword);
-      },
-      error: function (error) {
-        alert("Failed to reset password");
-      },
-    });
-  });
-});
+    // Show confirmation dialog
+    var confirmed = confirm('Are you sure you want to reset the password?')
+
+    // If user confirms, proceed with the AJAX request
+    if (confirmed) {
+      $.ajax({
+        url: '/admin/resetPassword',
+        type: 'POST',
+        data: JSON.stringify({ userId: userId }),
+        contentType: 'application/json',
+        dataType: 'json',
+        success: function (response) {
+          alert('New Password: ' + response.newPassword)
+        },
+        error: function (error) {
+          alert(error)
+        }
+      })
+    }
+  })
+})
--- public/js/search.js
+++ public/js/search.js
@@ -1,51 +1,54 @@
+/* eslint-env browser, jquery */
+/* global moment */
+
 $(document).ready(function () {
-  var searchInput = $("#search-input");
-  var searchResults = $("#search-results");
+  var searchInput = $('#search-input')
+  var searchResults = $('#search-results')
 
-  $(".search-modal").on("shown.bs.modal", function () {
-    searchInput.focus().select();
-  });
+  $('.search-modal').on('shown.bs.modal', function () {
+    searchInput.focus().select()
+  })
 
-  searchInput.on("input", function () {
-    var query = searchInput.val().trim();
+  searchInput.on('input', function () {
+    var query = searchInput.val().trim()
 
     if (query.length > 0) {
       $.ajax({
-        url: "/api/search",
-        method: "GET",
+        url: '/api/search',
+        method: 'GET',
         data: { q: query },
         success: function (data) {
-          searchResults.empty();
+          searchResults.empty()
 
           data.forEach(function (note) {
-            var listItem = $("<li></li>");
+            var listItem = $('<li></li>')
 
-            var link = $("<a></a>")
-              .attr("href", "/" + note.shortid)
-              .attr("target", "_blank");
+            var link = $('<a></a>')
+              .attr('href', '/' + note.shortid)
+              .attr('target', '_blank')
 
-            var title = $("<h3></h3>").text(note.title);
-            var lastchangeAt = $("<div></div>")
-              .text(moment(note.lastchangeAt).format("YYYY-MM-DD HH:mm"))
-              .css("float", "right");
-            var content = $("<div></div>");
+            var title = $('<h3></h3>').text(note.title)
+            var lastchangeAt = $('<div></div>')
+              .text(moment(note.lastchangeAt).format('YYYY-MM-DD HH:mm'))
+              .css('float', 'right')
+            var content = $('<div></div>')
             note.highlighted_content.forEach(function (highlight) {
-              var paragraph = $("<p></p>").html(highlight);
-              content.append(paragraph);
-            });
+              var paragraph = $('<p></p>').html(highlight)
+              content.append(paragraph)
+            })
 
-            link.append(lastchangeAt).append(title).append(content);
-            listItem.append(link);
+            link.append(lastchangeAt).append(title).append(content)
+            listItem.append(link)
 
-            searchResults.append(listItem);
-          });
+            searchResults.append(listItem)
+          })
         },
         error: function (jqXHR, textStatus, errorThrown) {
-          console.error("error:", textStatus, errorThrown);
-        },
-      });
+          console.error('error:', textStatus, errorThrown)
+        }
+      })
     } else {
-      searchResults.empty();
+      searchResults.empty()
     }
-  });
-});
+  })
+})
